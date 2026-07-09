6bd0c31bf215394c34e03e0bbaaec0df9b9789e2
Ben Chen <idben.chen@gmail.com>
2026-07-09 14:29:18 +0800

    各種取值與錯誤防範

--- dict1.py
+++ dict1.py
@@ -29,3 +29,24 @@ values = [1, 2]
 print(list(zip(keys, values)))
 d6 = dict(zip(keys, values))
 print("d6", d6)
+print("-"*30)
+
+# 取值
+print("d6 的 k1 是", d6["k1"])
+if "k3" in d6:
+    print("d6 的 k3 是", d6["k3"])    # 用 [] 取字典內容時, key 不存在, 會丟出錯誤
+
+print("d6 的 k3 是", d6.get("k3"))    # 使用 .get() 取用字典內容，不存在時會回傳 None
+print("d6 的 k3 是", d6.get("k3", 0)) # 使用 .get() 取用字典內容，可以設定不存在時回傳的值，也叫預設值
+
+# try-except 會補獲執行階段的錯誤
+try:
+    print("d6 的 k3 是", d6["k3"])
+    a5 = d6["k3"]
+except:
+    print("剛剛有錯誤產生")
+    a5 = None
+
+print("a5", a5)
+v1 = d6.setdefault("k4", 666)
+print("v1", v1)
