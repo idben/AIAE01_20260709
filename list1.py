08b0cf802542f2d4086f23da35b5193fabe99aaf
Ben Chen <idben.chen@gmail.com>
2026-07-09 09:28:00 +0800

    sort key lambda + 真實資料

--- list1.py
+++ list1.py
@@ -9,3 +9,19 @@ list2 = ["cc", "b", "aaa"]
 list2.sort()
 list2.sort(key=len)
 print(list2)
+
+# sort key lambda
+# lambda 匿名函式
+# 公式:  lambda 參數名: 公式
+words = ["banana", "apple", "cherry"]
+words.sort(key=lambda word: word[2])
+print(words)
+
+# sort key lambda + 真實資料
+datas = [
+    {"id": 1, "name": "Ben", "age": 38},
+    {"id": 2, "name": "May", "age": 18},
+    {"id": 3, "name": "John", "age": 28}
+]
+datas.sort(key=lambda data: data["age"], reverse=True)
+print(datas)
